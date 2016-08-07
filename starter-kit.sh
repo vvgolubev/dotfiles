c89470aa69c5e8fc754d46e5a9ee602d67718cb5
OvaUbe <trevorismay@gmail.com>
2016-08-07 21:54:21 +0300

    Add starter-kit

---
+++ starter-kit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pacaur -S \ # official repos
+	zsh \
+	vim \
+	sddm \
+	sxhkd \
+	clementine \
+	bspwm \
+	xmobar \
+	tmux \
+	xorg-server xorg-server-utils xorg-xinit \
+	chromium \
+	gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav gvfs \
+	network-manager-applet \
+	trayer \
+	deluge \
+	\ # aur
+	sublime-text-dev \
+	telegram-desktop \
+	
