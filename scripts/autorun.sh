3e8358fb6646bd6872a309ef6b18d6c229ee5717
OvaUbe <trevorismay@gmail.com>
2016-09-13 22:57:17 +0300

    add autorun and execute autorun in bspwmrc

---
+++ scripts/autorun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+feh --bg-scale ~/dotfiles/look/Wallpapers/solid-000000-1920x1080.jpg &
+~/bin/gifbg.sh ~/dotfiles/look/Wallpapers/beesnbombs-worms.gif &
+telegram-desktop &
+deluge &
+cbatticon &
+nm-applet &
+xset b off &
+unclutter -idle 2 &
