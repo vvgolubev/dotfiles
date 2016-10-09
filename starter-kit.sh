a810ed78ad3b74d3ab923306b17602d9cf65f793
OvaUbe <trevorismay@gmail.com>
2016-10-09 10:37:11 +0300

    Fix starter-kit

--- starter-kit.sh
+++ starter-kit.sh
@@ -3,14 +3,14 @@
 install_from_aur() {
 	wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
 	tar -xvf $1.tar.gz
-	cd $1 && makepkg -sri --skippgpcheck
+	cd $1 && makepkg -sri --skippgpcheck --noconfirm
 	cd .. && rm -rf $1.tar.gz $1
 }
 
 install_from_aur cower
 install_from_aur pacaur
 
-pacaur -S echo `cat paclist`
+pacaur -S `cat paclist` --noconfirm
 
 PREV=$PWD
 cd ~/
