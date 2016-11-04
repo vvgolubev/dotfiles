1df07bb1a05b4b43f7f6e1e1c7b11b3830217a98
OvaUbe <trevorismay@gmail.com>
2016-11-04 12:59:45 +0300

    Change Wallpaper

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-feh --bg-scale ~/dotfiles/look/Wallpapers/solid-000000-1920x1080.jpg &
-~/bin/gifbg.sh ~/dotfiles/look/Wallpapers/beesnbombs-worms.gif &
+feh --bg-scale ~/dotfiles/look/Wallpapers/solid-2A2826-1920x1080.jpg &
+~/bin/gifbg.sh ~/dotfiles/look/Wallpapers/beesnbombs-necklace.gif &
 telegram-desktop &
 deluge &
 cbatticon &
