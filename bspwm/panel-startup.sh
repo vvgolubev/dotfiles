0f2960eed90048df7c6b3ac270e29be76afb04f6
OvaUbe <trevorismay@gmail.com>
2017-01-08 16:50:09 +0300

    Remove unused pipe.

--- bspwm/panel-startup.sh
+++ bspwm/panel-startup.sh
@@ -2,7 +2,6 @@
 
 bspc config top_padding $BSPWM_PANEL_HEIGHT
 
-[[ -e "$BSPWM_PANEL_FIFO"  ]] || mkfifo "$BSPWM_PANEL_FIFO"
 [[ -e "$BSPWM_VOLUME_FIFO" ]] || mkfifo "$BSPWM_VOLUME_FIFO"
 [[ -e "$BSPWM_BATTERY_FIFO" ]] || mkfifo "$BSPWM_BATTERY_FIFO"
 
