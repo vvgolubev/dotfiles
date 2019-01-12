f99d02454c530e79c2ede0cfa080ca5584f3539a
Vladimir Golubev <subsinthe@gmail.com>
2019-01-12 20:19:59 +0000

    Add yandex and mongo apt sources

--- starter-kit.sh
+++ starter-kit.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+sudo ln -vfs ${PWD}/apt/sources.list.d /etc/apt/sources.list.d
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add -
+
+sudo apt update
 sudo apt-get install `cat paclist` --force-yes
 
 PREV=$PWD
